f6906e8561cecfad2a5c98529c5e74bf6c32aa63
Leisure <153300677+leisureprog@users.noreply.github.com>
2025-08-06 14:27:24 +0700

    Update index.ts

--- pages/content-ui/src/index.ts
+++ pages/content-ui/src/index.ts
@@ -1,6 +1,5 @@
 import { createApp, defineComponent, h } from 'vue'
 import App from '@src/App.vue'
-// @ts-expect-error Because file doesn't exist before build
 import tailwindcssOutput from '../dist/tailwind-output.css?inline'
 
 const root = document.createElement('div')
