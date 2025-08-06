c65ae73873e1e552b9fa542b7615d70a7db0e444
Leisure <153300677+leisureprog@users.noreply.github.com>
2025-08-06 14:36:39 +0700

    Update index.ts

--- chrome-extension/src/background/index.ts
+++ chrome-extension/src/background/index.ts
@@ -1,12 +1,9 @@
 import 'webextension-polyfill'
-import { startHighlander } from './highlander'
 import { startExtension } from './extension'
 
 console.log('background loaded')
 
 async function main() {
-  startHighlander()
-  
   startExtension()
 }
 
